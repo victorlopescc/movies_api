6c3d4627165516d368aeceb76c695fe20cdc17b1
victorlopescc <victorlopes.cc@gmail.com>
2023-12-14 17:27:46 -0300

    feat: Notes show

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -25,7 +25,17 @@ class NotesController {
         response.json();
     }
 
-    async show(request, response) { }
+    async show(request, response) {
+        const { id } = request.params;
+
+        const note = await knex('notes').where({ id }).first();
+        const tags = await knex('tags').where({ note_id: id }).orderBy('name');
+
+        return response.json({
+            ...note,
+            tags
+        });
+    }
 
     async delete(request, response) { }
 
